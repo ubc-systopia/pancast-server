3583bd72f3fba0366663ef4707e545d7093422d6
Ming <mingjiang@live.nl>
2021-06-10 14:28:07 -0700

    saving work (current branch will fail)

---
+++ cmd/app/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	server "pancast-server"
+	"pancast-server/config"
+)
+
+
+
+func main() {
+	var conf config.StartParameters
+	err := config.ReadJSONConfig("config/app_config.json", &conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	srv, done := server.StartServer(config.GetServerURL(conf))
+	<-done
+	fmt.Println("\nShutting down...")
+	err = srv.Shutdown(context.Background())
+	if err != nil {
+		panic(err)
+	}
+}
